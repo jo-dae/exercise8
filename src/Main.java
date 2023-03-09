93c7953aa4839fb1953a364dc05ae18d26ea9f32
jo-dae <117208717+jo-dae@users.noreply.github.com>
2023-03-09 14:14:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -53,3 +53,28 @@ class Person{
                 '}';
     }
 }
+
+
+
+class Number{
+    private int number;
+
+    public Number(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Number{" +
+                "number=" + number +
+                '}';
+    }
+}
