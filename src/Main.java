046de9e794f8d4a84e5585e32eb17841cb1c2bfd
Jo-Dae J Lowe 11th CCHS <117494@SWCSD.US>
2023-03-09 14:04:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Person john = new Person("john", "Johnmail", 675171831);
+        System.out.println(john);
+
+    }
+}
+
+class Person{
+    private String name;
+    private String email;
+    private int number;
+
+    public Person(String name, String email, int number) {
+        this.name = name;
+        this.email = email;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
